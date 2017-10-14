f7abbc0840ab382764d2bc753d83c68389b81acb
jdburge <j2burge@comcast.net>
2017-10-13 23:39:50 -0500

    added dt-learn.py

---
+++ CS760Hw2/dt-learn.py
@@ -0,0 +1,14 @@
+import sys
+from scipy.io import arff
+import ID3
+
+data, meta = arff.loadarff(sys.argv[0])
+test_data, test_meta = arff.loadarff(sys.argv[1])
+
+m = sys.argv[2]
+
+myID3 = ID3.ID3Classifier(m)
+
+myID3.train(data, meta, 'class')
+myID3.print_tree()
+myID3.test(test_data, 'class', printing=True)
